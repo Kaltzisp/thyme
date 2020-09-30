2475c35202895b5d4c429df2e2d8a7239bd9872b
Peter Kaltzis <pkal0002@student.monash.edu>
2020-09-30 13:02:32 +1000

    marriage list bugfix

--- libs/text/marriage.js
+++ libs/text/marriage.js
@@ -7,11 +7,12 @@ module.exports.list = async function(msg) {
         embed.setTitle(`**${msg.member.user.tag} has no active marriages!**`);
     } else {
         embed.setColor("#0099ff");
-        embed.setTitle(`:revolving_hearts: **Marriages of ${msg.member.user.tag}`);
-        embed.setImage(msg.member.user.avatarURL());
+        embed.setTitle(`:revolving_hearts: **Marriages of ${msg.member.user.tag}**`);
+        embed.setThumbnail(msg.member.user.avatarURL());
         for (const i in msg.client.userSave[msg.member.id].marriages) {
             const user = await msg.client.users.fetch(i).catch((err) => console.log(err));
-            embed.addField(user.tag, `Since ${msg.client.userSave[msg.member.id].marriages[i].toDateString().substring(3)}.`);
+            const newDate = new Date(msg.client.userSave[msg.member.id].marriages[i]);
+            embed.addField(user.tag, newDate.toDateString().substring(3));
         }
     }
     msg.channel.send(embed);
