4a66c521f2aa7708f455d6a0f7368505d7c962f8
Peter Kaltzis <pkal0002@student.monash.edu>
2020-10-13 02:53:40 +1100

    reorder keys

--- libs/server/auth.js
+++ libs/server/auth.js
@@ -23,14 +23,14 @@ try {
     module.exports = {
         discord: process.env.discord,
         jsonbin: process.env.jsonbin,
+        ksoft: process.env.ksoft,
+        spotify_id: process.env.spotify_id,
+        spotify_secret: process.env.spotify_secret,
         weatherio: process.env.weatherio,
         youtube_1: process.env.youtube_1,
         youtube_2: process.env.youtube_2,
         youtube_3: process.env.youtube_3,
-        youtube_4: process.env.youtube_4,
-        spotify_id: process.env.spotify_id,
-        spotify_secret: process.env.spotify_secret,
-        ksoft: process.env.ksoft
+        youtube_4: process.env.youtube_4
     };
 }
 
