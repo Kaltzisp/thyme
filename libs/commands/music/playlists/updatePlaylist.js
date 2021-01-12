097aebf0ab706a0aa8be6ee6b3854931ec0e6348
Peter Kaltzis <pkal0002@student.monash.edu>
2021-01-12 11:11:52 +1100

    playlist update errormsg

--- libs/commands/music/playlists/updatePlaylist.js
+++ libs/commands/music/playlists/updatePlaylist.js
@@ -7,14 +7,18 @@ module.exports = {
     args: ["<playlist_index OR playlist_title>"],
     exe(msg) {
         const index = getIndex(msg);
-        if (msg.client.save.playlists[index] && msg.guild.queue.length > 0) {
-            msg.client.save.playlists[index][1].length = 0;
-            for (const i in msg.guild.queue) {
-                msg.client.save.playlists[index][1].push(msg.guild.queue[i]);
+        if (msg.client.save.playlists[index]) {
+            if (msg.guild.queue.length > 0) {
+                msg.client.save.playlists[index][1].length = 0;
+                for (const i in msg.guild.queue) {
+                    msg.client.save.playlists[index][1].push(msg.guild.queue[i]);
+                }
+                msg.send(`Playlist updated\nID: \`${index}\`\nName: \`${msg.client.save.playlists[index][0]}\`.`);
+            } else {
+                msg.send("Cannot create playlist, as the queue is empty!");
             }
-            msg.send(`Playlist updated\nID: \`${index}\`\nName: \`${msg.client.save.playlists[index][0]}\`.`);
         } else {
-            msg.send("Cannot create playlist, as the queue is empty!");
+            msg.send(`Playlist \`${index}\` not found!`);
         }
     }
 };
