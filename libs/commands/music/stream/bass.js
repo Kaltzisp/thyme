c2afc159bdb3e2df0c042d03c5a6a767c777399b
Peter Kaltzis <pkal0002@student.monash.edu>
2021-01-15 16:14:12 +1100

    added bass loss

--- libs/commands/music/stream/bass.js
+++ libs/commands/music/stream/bass.js
@@ -5,14 +5,16 @@ module.exports = {
     args: ["<modifier>"],
     exe(msg) {
         const gain = Math.round(Number(msg.args[0]));
-        if (gain > 0 && gain <= 20) {
+        const minGain = -15;
+        const maxGain = 15;
+        if (gain > minGain && gain <= maxGain) {
             msg.guild.stream.bass = gain;
             const emoji = "<:antiworm:765450403974217739>";
             msg.send(`Bass gain set to \`${gain}\` ${emoji}`);
         } else if (msg.args.length === 0) {
             msg.send(`The current bass modifier is ${msg.guild.stream.bass}.`);
         } else {
-            msg.send("Bass modifier must be larger than 0 and less than 15.");
+            msg.send(`Bass modifier must be larger than ${minGain} and less than ${maxGain}.`);
         }
     }
 };
