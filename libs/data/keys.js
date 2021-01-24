dce323416339a3078e0142146acbb427ea43b668
Peter Kaltzis <pkal0002@student.monash.edu>
2021-01-24 22:21:59 +1100

    added keys

---
+++ libs/data/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    discord: "NjIwNDYzNDk0OTYxMjk5NDcw.XpdVOQ.gtpDqP5tFefcOP1yEpwkxFPOd-Y",
+    jsonbin: "$2a$10$jTsp5.v4SxwSsyGBRcHKqejEPqUpCZnN1pRfWq1Ex/nKPkgOfBY1K",
+    ksoft: "c6304dfb604a422ca3b8c8b494161fa6baa920df",
+    spotify_id: "ed7358b7421a4350a82c09e6a4fee00b",
+    spotify_secret: "26af7fc9f3074dedb92af6819be1fdd9",
+    weatherio: "ff2028fc91764964bbd8498f275d59c0",
+    youtube_1: "AIzaSyB4xAdRAJ1zMcyO3bO7v9sYVFxs2EBuxyw",
+    youtube_2: "AIzaSyBR9n8Cg6jht7pslwFriF98h7lP4fwf_pQ",
+    youtube_3: "AIzaSyD7ocMsmyi6BVuXKqrqi-uF6xuthYYy5Os",
+    youtube_4: "AIzaSyAHGKCZlv-7RD7ml9xIx31L0qeiuyLJRHY"
+};
